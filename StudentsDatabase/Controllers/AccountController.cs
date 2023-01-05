bf7330c8f73d1c734f8307e722e733684738d55e
theothernur <eveandlight@gmail.com>
2023-01-05 18:41:44 +0300

    Add project files.

---
+++ StudentsDatabase/Controllers/AccountController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StudentsDatabase.Helper;
+using StudentsDatabase.ViewModels;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
+
+namespace StudentsDatabase.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IConfiguration _config;
+        CommonHelper _helper;
+
+        void AccountsController(IConfiguration config)
+        {
+            _config = config;
+            _helper = new CommonHelper(_config);
+
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Register (RegisterStudents vm)
+        {
+            string UserExistsQuery = $"Select * from [StudentsDb] where Username='{vm.Username}'"+ $" OR Email='{vm.Email}'";
+            bool userExists = _helper.UserAlreadyExists(UserExistsQuery);
+
+            if (userExists == true)
+            {
+                ViewBag.Error = "Username and Email Already Exists";
+                return View("Register", "LoginPartial");
+            }
+
+            string Query = "Insert into [StudentsDb] (Username, Email, Password, Name," + 
+                $"Contact)values('{vm.Username}', '{vm.Email}', '{vm.Password}', '{vm.Name}' ,'{vm.Contact}')";
+
+            int result = _helper.DMLTransaction(Query);
+            if (result > 0)
+            {
+                EntryIntoSession(vm.Username);
+                return RedirectToAction("Index", "Default");
+            }
+            return View();
+
+        }
+
+
+        private void EntryIntoSession(string Username)
+        {
+            HttpContext.Session.SetString("Username", Username);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ StudentsDatabase/Helper/CommonHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+namespace StudentsDatabase.Helper
+{
+    public class CommonHelper
+    {
+        private IConfiguration _config;
+        
+            public CommonHelper(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public int DMLTransaction(string Query)
+        {
+            int Result;
+            string connectionString = _config["ConnectionStrings:DefaultConnection"];
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sql = Query;
+                SqlCommand com = new SqlCommand(sql, connection);
+                Result = com.ExecuteNonQuery();
+                connection.Close();
+            }
+            return Result;
+
+        }
+
+
+
+        public bool UserAlreadyExists(string query)
+        {
+            bool flag = false;
+            string connectionString = _config["ConnectionStrings:DefaultConnection"];
+
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sql = query;
+                SqlCommand com = new SqlCommand(sql, connection);
+                SqlDataReader rd = com.ExecuteReader();
+                if(rd.HasRows)
+                {
+                    flag = true;
+                }
+                connection.Close();
+            }
+            return flag;
+        }
+
+
+    }
+}
